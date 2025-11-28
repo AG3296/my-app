329e0c2a53509ed7c078e2f2f1961983e2e010df
AG3296 <AG3296@student.jamk.fi>
2025-11-28 23:08:26 +0200

    Exercise_14_Publish_App-Done

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "right",   // horizontal center
+        alignItems: "right",       // vertical center
+        height: "100vh",            // full viewport height
+        fontSize: "2rem",
+        textAlign: "right",
+        
+      }}
+    >
+      Hello GitLab - This works!
+    </div>
+  );
+}
+
+export default App;
